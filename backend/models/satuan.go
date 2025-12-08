7e2996062a1fc54138c02550cbba6b2b2ebb6407
SellaSakdiah <shelasakdiah2201@gmail.com>
2025-12-08 20:33:29 +0700

    update satuan.go

--- backend/models/satuan.go
+++ backend/models/satuan.go
@@ -1,7 +1,7 @@
 package models
 
 type Satuan struct {
-	IDSatuan    int    `gorm:"column:idSatuan;primaryKey" json:"id_satuan"`
+	IdSatuan    int    `gorm:"column:idSatuan;primaryKey" json:"id_satuan"`
 	JenisSatuan string `gorm:"column:jenis_satuan" json:"jenis_satuan"`
 }
 
