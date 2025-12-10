d93279b7c69cbf621f56f5edf6fc09886464007b
SellaSakdiah <shelasakdiah2201@gmail.com>
2025-12-10 16:33:09 +0700

    update gudang go

--- backend/models/gudang.go
+++ backend/models/gudang.go
@@ -3,7 +3,7 @@ package models
 type Gudang struct {
 	IdGudang   int    `gorm:"column:idGudang;primaryKey" json:"id_gudang"`
 	NamaGudang string `gorm:"column:nama_gudang" json:"nama_gudang"`
-	Alamat     string `gorm:"column:alamat" json:"alamat"`
+	Alamat     string `gorm:"column:alamat_gudang" json:"alamat_gudang"`
 }
 
 func (Gudang) TableName() string {
